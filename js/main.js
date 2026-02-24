574a376428cadd394e9dee64f1aa4f2758668061
milinera <milineragrov@gmail.com>
2026-02-24 21:12:20 +0300

    less 63

---
+++ js/main.js
@@ -0,0 +1,42 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none'
+        });
+
+        tabs.forEach(tab => {
+            tab.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const targer = event.target;
+
+        if (targer && targer.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (targer == item) {
+                        hideTabContent();
+                        showTabContent(i);
+                }
+            })
+        }
+    });
+
+
+
+
+
+})

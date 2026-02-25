109bda1c377dc9c18abbbb40e4c18d10db3f10be
milinera <milineragrov@gmail.com>
2026-02-25 19:39:20 +0300

    add timer

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+    //tabs
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -34,9 +34,67 @@ window.addEventListener('DOMContentLoaded', () => {
             })
         }
     });
+    const links = document.querySelectorAll('.header__link');
+    links[1].style.display = 'none';
+    const timer = setInterval(longer, 3000);
+
+    function longer() {
+        links[1].style.display = 'block';
+    }
+    links[0].addEventListener('click', () => {
+        links[1].style.display = 'none';
+    })
 
 
+    //timer
+    const deadLine = '2026-02-26';
 
 
+    function getTime(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+              minuts = Math.floor((t / (1000 * 60)) % 60),
+              sec = Math.floor((t / 1000) % 60);
+
+        return {
+            'total' : t,
+            'days' : days,
+            'housr' : hours,
+            'min' : minuts,
+            'sec' : sec
+         }
+    }
+
+    function getZiro(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minuts = timer.querySelector('#minutes'),
+              sec = timer.querySelector('#seconds'),
+              timeInt = setInterval(uploudTime, 1000);
+        uploudTime();
+        function uploudTime() {
+            const t = getTime(endtime);
+            days.innerHTML = getZiro(t.days);
+            hours.innerHTML = getZiro(t.housr);
+            minuts.innerHTML = getZiro(t.min);
+            sec.innerHTML = getZiro(t.sec);
+
+            if (t.total <= 0) {
+                clearInterval(timeInt);
+            }
+        }
+    }
+
+    setClock('.timer', deadLine);
 
 })
